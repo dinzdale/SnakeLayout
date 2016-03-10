8ac7b5112e0d8793a675914df3c9e4a83fce1b2c
Keith  Jacobs <keithjacobs@Keiths-MacBook-Pro.local>
2016-03-09 22:14:55 -0500

    Update Model as observable

--- app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/MainActivity.java
+++ app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/MainActivity.java
@@ -3,6 +3,7 @@ package com.snakelayout.gary_jacobs.infragisiticstasksnakelayout;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,31 +15,39 @@ public class MainActivity extends AppCompatActivity {
 
     SnakeLayout snakeLayout;
     ButtonModel buttonModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         buttonModel = new ButtonModel();
         buttonModel.setButtonLabel("Button Label Set!");
         binding.setButtonVM(buttonModel);
         snakeLayout = (SnakeLayout) findViewById(R.id.snake_layout);
 
-        //load();
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         buttonModel.setButtonLabel("On Resume!!!");
+        loadData.start();
     }
 
-    protected void load() {
-        super.onResume();
-        for (int i = 0; i < 100; i++) {
-            TextView textView = new TextView(this);
-            textView.setText("TextView " + i);
-            snakeLayout.addView(textView);
+
+    Thread loadData = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                buttonModel.setButtonLabel("Button Label " + i);
+                try {
+                    Thread.sleep(1000 * 5);
+                } catch (Exception ex) {
+                    Log.d("10", "Error in Thread", ex);
+                }
+            }
         }
-    }
+    });
 }
--- app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/model/ButtonModel.java
+++ app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/model/ButtonModel.java
@@ -1,17 +1,26 @@
 package com.snakelayout.gary_jacobs.infragisiticstasksnakelayout.model;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.snakelayout.gary_jacobs.infragisiticstasksnakelayout.BR;
+
+import java.util.Observable;
+
 /**
  * Created by Gary Jacobs on 3/9/16.
  */
-public class ButtonModel  {
+public class ButtonModel extends BaseObservable {
 
     private String buttonLabel = "buttonLabel";
 
+    @Bindable
     public String getButtonLabel() {
         return buttonLabel;
     }
 
     public void setButtonLabel(String buttonLabel) {
         this.buttonLabel = buttonLabel;
+        notifyPropertyChanged(BR.buttonLabel);
     }
 }
