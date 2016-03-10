c04d9b4018458b2bda1a52b65cf8ef853516b682
Keith  Jacobs <keithjacobs@Keiths-MacBook-Pro.local>
2016-03-09 21:00:20 -0500

    Initial MVVM implementation.

--- app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/MainActivity.java
+++ app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/MainActivity.java
@@ -1,23 +1,37 @@
 package com.snakelayout.gary_jacobs.infragisiticstasksnakelayout;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.snakelayout.gary_jacobs.infragisiticstasksnakelayout.databinding.ActivityMainBinding;
+import com.snakelayout.gary_jacobs.infragisiticstasksnakelayout.model.ButtonModel;
+
 public class MainActivity extends AppCompatActivity {
 
     SnakeLayout snakeLayout;
-
+    ButtonModel buttonModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        buttonModel = new ButtonModel();
+        buttonModel.setButtonLabel("Button Label Set!");
+        binding.setButtonVM(buttonModel);
         snakeLayout = (SnakeLayout) findViewById(R.id.snake_layout);
-        load();
+
+        //load();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        buttonModel.setButtonLabel("On Resume!!!");
+    }
 
     protected void load() {
         super.onResume();
---
+++ app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/model/ButtonModel.java
@@ -0,0 +1,17 @@
+package com.snakelayout.gary_jacobs.infragisiticstasksnakelayout.model;
+
+/**
+ * Created by Gary Jacobs on 3/9/16.
+ */
+public class ButtonModel  {
+
+    private String buttonLabel = "buttonLabel";
+
+    public String getButtonLabel() {
+        return buttonLabel;
+    }
+
+    public void setButtonLabel(String buttonLabel) {
+        this.buttonLabel = buttonLabel;
+    }
+}
