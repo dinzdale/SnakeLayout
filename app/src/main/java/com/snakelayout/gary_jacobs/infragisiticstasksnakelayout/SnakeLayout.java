15406f7bef4fbdfaa31229f9a7c1890386b480b9
Keith  Jacobs <keithjacobs@Keiths-MacBook-Pro.local>
2016-03-12 18:04:31 -0500

    Add snakelayout and remove other layouts.

--- app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/SnakeLayout.java
+++ app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/SnakeLayout.java
@@ -17,6 +17,8 @@ import android.view.WindowManager;
  */
 public class SnakeLayout extends ViewGroup {
 
+    // Defines the wrapping direction for the child views when positioning the views
+    // within the layout
     public static enum Orientation {
         WRAP_LEFT_TO_RIGHT(0), WRAP_RIGHT_TO_LEFT(1);
         int value;
@@ -27,7 +29,7 @@ public class SnakeLayout extends ViewGroup {
     }
 
     private Orientation orientation = Orientation.WRAP_RIGHT_TO_LEFT;
-    private int deviceWidth = 1;
+    private int deviceWidth;
 
     public SnakeLayout(Context context) {
         super(context, null, 0);
@@ -48,7 +50,7 @@ public class SnakeLayout extends ViewGroup {
         Point deviceDimensions = new Point();
         display.getSize(deviceDimensions);
         deviceWidth = deviceDimensions.x;
-        setOrientation(Orientation.WRAP_LEFT_TO_RIGHT);
+        setOrientation(Orientation.WRAP_RIGHT_TO_LEFT);
     }
 
 
@@ -74,19 +76,21 @@ public class SnakeLayout extends ViewGroup {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         switch (getOrientation()) {
             case WRAP_LEFT_TO_RIGHT:
-                layoutForLeftRightSnake(changed, l, t, r, b);
+                layoutSnake(changed, l, t, r, b, true);
                 break;
             case WRAP_RIGHT_TO_LEFT:
-                layoutForRightLeft(changed, l, t, r, b);
+                layoutSnake(changed, l, t, r, b, false);
                 break;
         }
     }
 
-    private void layoutForLeftRight(boolean changed, int l, int t, int r, int b) {
+
+    private void layoutSnake(boolean changed, int l, int t, int r, int b, boolean orientation) {
         final int count = getChildCount();
+        boolean leftToRight = orientation;
         int curWidth, curHeight, curLeft, curTop, maxHeight;
 
-        //get the available size of child view
+        //get the starting position of each child
         final int childLeft = this.getPaddingLeft();
         final int childTop = this.getPaddingTop();
         final int childRight = this.getMeasuredWidth() - this.getPaddingRight();
@@ -95,52 +99,17 @@ public class SnakeLayout extends ViewGroup {
         final int childHeight = childBottom - childTop;
 
         maxHeight = 0;
-        curLeft = childLeft;
-        curTop = childTop;
 
-        for (int i = 0; i < count; i++) {
-
-            View child = getChildAt(i);
-
-            if (child.getVisibility() == GONE)
-                return;
-
-            //Get the maximum size of the child
-            child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
-            curWidth = child.getMeasuredWidth();
-            curHeight = child.getMeasuredHeight();
-            //wrap is reach to the end
-            if (curLeft + curWidth >= childRight) {
-                curLeft = childLeft;
-                curTop += maxHeight;
-                maxHeight = 0;
-            }
-            //do the layout
-            child.layout(curLeft, curTop, curLeft + curWidth, curTop + curHeight);
-            //store the max height
-            if (maxHeight < curHeight)
-                maxHeight = curHeight;
-            curLeft += curWidth;
+        // initialize current position depending left to right/right to left orientation
+        if (leftToRight) {
+            curLeft = childLeft;
+        } else {
+            curLeft = childRight;
         }
-    }
 
-    private void layoutForLeftRightSnake(boolean changed, int l, int t, int r, int b) {
-        boolean leftToRight = true;
-        final int count = getChildCount();
-        int curWidth, curHeight, curLeft, curTop, maxHeight;
-
-        //get the available size of child view
-        final int childLeft = this.getPaddingLeft();
-        final int childTop = this.getPaddingTop();
-        final int childRight = this.getMeasuredWidth() - this.getPaddingRight();
-        final int childBottom = this.getMeasuredHeight() - this.getPaddingBottom();
-        final int childWidth = childRight - childLeft;
-        final int childHeight = childBottom - childTop;
-
-        maxHeight = 0;
-        curLeft = childLeft;
         curTop = childTop;
 
+        // For each child in the layout, determine it's proper layout position
         for (int i = 0; i < count; i++) {
 
             View child = getChildAt(i);
@@ -148,31 +117,41 @@ public class SnakeLayout extends ViewGroup {
             if (child.getVisibility() == GONE)
                 return;
 
-            //Get the maximum size of the child
+            // Get the width and height of the next child
             child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
             curWidth = child.getMeasuredWidth();
             curHeight = child.getMeasuredHeight();
-            //wrap is reach to the end
+
+            // position this child view
             if (leftToRight) {
+                // keep moving left to right until we run out of room
                 if (curLeft + curWidth >= childRight) {
+                    // no more room on this line, move child down and start moving right to left (snake!)
                     curLeft = childRight - curWidth;
+                    // position top of this view below tallest view on previous line
                     curTop += maxHeight;
                     maxHeight = 0;
                     leftToRight = false;
                 }
             } else {
+                // keep moving right to left until we run out of room
                 if (curLeft - curWidth < childLeft) {
+                    // no more room on this line, move child down and start moving left to right again
                     curLeft = childLeft;
+                    // position top of this view below tallest view on previous line
                     curTop += maxHeight;
                     maxHeight = 0;
                     leftToRight = true;
                 }
             }
-            //do the layout
+            //render this child view
             child.layout(curLeft, curTop, curLeft + curWidth, curTop + curHeight);
-            //store the max height
+
+            //save the maximum height, or tallest height view in the current row
             if (maxHeight < curHeight)
                 maxHeight = curHeight;
+
+            // position the next view left position for the next child
             if (leftToRight) {
                 curLeft += curWidth;
             } else {
@@ -181,51 +160,12 @@ public class SnakeLayout extends ViewGroup {
         }
     }
 
-    private void layoutForRightLeft(boolean changed, int l, int t, int r, int b) {
-        final int count = getChildCount();
-        int curWidth, curHeight, curRight, curTop, maxHeight;
 
-        //get the available size of child view
-        final int childRight = this.getMeasuredWidth() - this.getPaddingRight();
-        final int childTop = this.getPaddingTop();
-        final int childLeft = this.getPaddingLeft();
-        final int childBottom = this.getMeasuredHeight() - this.getPaddingBottom();
-        final int childWidth = childLeft - childRight;
-        final int childHeight = childBottom - childTop;
-
-        maxHeight = 0;
-        curRight = childLeft - childWidth;
-        curTop = childTop;
-
-        for (int i = 0; i < count; i++) {
-
-            View child = getChildAt(i);
-
-            if (child.getVisibility() == GONE)
-                return;
-
-            //Get the maximum size of the child
-            child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
-            curWidth = child.getMeasuredWidth();
-            curHeight = child.getMeasuredHeight();
-            //wrap is reach to the end
-            if (curRight - curWidth > childLeft) {
-                // layout button right to left, until end of line reached
-                curRight -= curWidth;
-            } else {
-                // end reached, wrap to next line
-                curRight = this.getMeasuredWidth() - this.getPaddingRight() - curWidth;
-                curTop += maxHeight;
-                maxHeight = 0;
-            }
-            //do the layout
-            child.layout(curRight, curTop, curRight + curWidth, curTop + curHeight);
-            //store the max height
-            if (maxHeight < curHeight)
-                maxHeight = curHeight;
-        }
-    }
-
+    /**
+     * System Callback requesting measurements of all children in this layout container
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
