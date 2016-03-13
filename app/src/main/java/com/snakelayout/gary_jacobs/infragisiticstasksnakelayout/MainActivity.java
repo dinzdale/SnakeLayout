f9586ca40bcbb66138dc9a56cb430925786afc1d
Keith  Jacobs <keithjacobs@Keiths-MacBook-Pro.local>
2016-03-13 14:31:52 -0400

    Fix snake layout and add "change orientation" button.

--- app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/MainActivity.java
+++ app/src/main/java/com/snakelayout/gary_jacobs/infragisiticstasksnakelayout/MainActivity.java
@@ -9,6 +9,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.snakelayout.gary_jacobs.infragisiticstasksnakelayout.databinding.ActivityMainBinding;
@@ -28,13 +29,13 @@ public class MainActivity extends AppCompatActivity {
         buttonModelView = new ButtonModelView();
         binding.setButtonVM(buttonModelView);
         snakeLayout = (SnakeLayout) findViewById(R.id.snake_layout);
-//        orientation_btn = (Button) findViewById(R.id.orientation_btn);
-//        orientation_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                snakeLayout.toggleOrientaton();
-//            }
-//        });
+        orientation_btn = (Button) findViewById(R.id.orientation_btn);
+        orientation_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snakeLayout.toggleOrientaton();
+            }
+        });
     }
 
     @Override
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             for (int i = 0; i < 3; i++) {
-                for (int x = 0; x < 15; x++) {
+                for (int x = 0; x < 17; x++) {
                     buttonModelView.setButtonValues(x, "Label Update " + (x + i), x % 2);
                 }
                 try {
